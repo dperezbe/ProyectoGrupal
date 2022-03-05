884300f5f49fe70e28e732361e996a460c73d47a
Niag2021 <arancibia.guerrero.nicolas@gmail.com>
2022-03-05 13:22:22 -0300

    Layout con css basico // Version 1.1

---
+++ client/src/components/index.js
@@ -0,0 +1,83 @@
+import { Container, Button, Col, Form, FormGroup, Input, Label, Row } from "reactstrap";
+import "../css/estiloIndex.css"
+//import Button from '@mui/material/Button';
+import { TextField } from "@mui/material";
+
+const Index = (props) => {
+    return (
+        //<></>
+        <Container>
+            <div id="redesSociales">
+                <button id="redesSociales2">F</button>
+                <button id="redesSociales2">Y</button>
+                <button id="redesSociales2">T</button>
+                <button id="redesSociales2">I</button>
+                <button id="redesSociales2">Tw</button>
+
+                <button id="login">Login</button>
+            </div>
+
+            <div id="parteIzquierda">
+                <div>
+
+                </div>
+            </div>
+            
+            <div id="parteDerecha">
+                <div>
+                    <Form id="formulario">
+                        <tr id="campos">
+                            <td id="texto"> Label 1</td>
+                            <td id="valor">Valor 1</td>
+                            <td id="acciones">Acciones</td>
+                        </tr>
+
+                        <tr id="campos">
+                            <td id="texto"> Label 2</td>
+                            <td id="valor">Valor 2</td>
+                            <td id="acciones">Acciones</td>
+                        </tr>
+
+                        <tr id="campos">
+                            <td id="texto"> Label 4</td>
+                            <td id="valor">Valor 4</td>
+                            <td id="acciones">Acciones</td>
+                        </tr>
+
+                        <tr id="campos">
+                            <td id="texto"> Label 5</td>
+                            <td id="valor">Valor 5</td>
+                            <td id="acciones">Acciones</td>
+                        </tr>
+
+                        <tr id="campos">
+                            <td id="texto"> Label 6</td>
+                            <td id="valor">Valor 6</td>
+                            <td id="acciones">Acciones</td>
+                        </tr>
+
+                        <tr id="campos">
+                            <td id="texto"> Label 7</td>
+                            <td id="valor">Valor 7</td>
+                            <td id="acciones">Acciones</td>
+                        </tr>
+
+                        <tr id="campos">
+                            <td id="texto"> Label 8</td>
+                            <td id="valor">Valor 8</td>
+                            <td id="acciones">Acciones</td>
+                        </tr>
+
+                        <tr id="campos">
+                            <td id="texto"> Label 9</td>
+                            <td id="valor">Valor 9</td>
+                            <td id="acciones">Acciones</td>
+                        </tr>
+                    </Form>
+                </div>
+            </div>
+        </Container>       
+    )
+}
+
+export default Index;

ac04e30b6d83b571c22c57b4bf64bc1a1d1f80b9
TheVale31 <valentinasanchez3101@gmailcom>
2022-03-05 18:08:13 -0300

    register v1

--- client/src/components/access/register.js
+++ client/src/components/access/register.js
@@ -6,18 +6,18 @@ import { Form, FormGroup , Label, Input, Button} from "reactstrap"
 const Register = props => {
 
     const initialValues = {
-        firstName: "",
-        lastName: "",
+        userName: "",
         email: "",
         password: "",
+        confirmPassword:"",
         birthdate: ""
     }
 
     const erroresIniciales = {
         firstName: "",
-        lastName: "",
         errEmail: "",
-        errPassword: ""
+        errPassword: "",
+        errConfirmPassword:"",
     }  
 
     const[inputs, setInputs]= useState(initialValues);
@@ -31,26 +31,27 @@ const Register = props => {
     }
 
     const handleChange = (e) => {
-        const {name, value} = e.target
-        setInputs({...inputs, [name]: value})
-        if (name === 'confirmPassword') {
-            if (inputs.password !== value) {
-                setErrors({
-                    ...errors,
-                    errConfirmPassword: "*Passwords must match"
-                });
-            } else {
-                setErrors({
-                    ...errors,
-                    errConfirmPassword: ''
-                });
-            }
-        }
+        // const {name, value} = e.target
+        // if (name === 'confirmPassword') {
+        //     if (inputs.password !== value) {
+        //         setErrors({
+        //             ...errors,
+        //             errConfirmPassword: "*Passwords must match"
+        //         });
+        //     } else {
+        //         setErrors({
+        //             ...errors,
+        //             errConfirmPassword: ''
+        //         });
+        //     }
+        // }
+        setInputs({...inputs, [e.target.name]:e.target.value})
+
     }
 
     const enviarRegistro = (e) => {
         e.preventDefault();
-        axios.post('http:/api/user',inputs)
+        axios.post('http://localhost:8000/api/user',inputs)
         .then(res=>{
             setInputs(initialValues)})
         .catch(err=>console.log(err))
@@ -62,23 +63,19 @@ const Register = props => {
             <p> Free Pickup Game Finder and Organizer</p>
             <Form onSubmit={handleSubmit}>
                 <FormGroup>
-                    <Label>Nombre:</Label>
-                    <Input name='firstname' value={inputs.firstName} onChange={handleChange}></Input>
-                    {errors['firstName'] ? <p className="errors_reg_log">{errors['firstName'].message}</p>: ''}             
+                    <Label>UserName:</Label>
+                    <Input name='username' onChange={handleChange}></Input>
+                    {errors['username'] ? <p className="errors_reg_log">{errors['username'].message}</p>: ''}             
                 </FormGroup>
-                <FormGroup>
-                    <Label>Apellido:</Label>
-                    <Input name='lastName' value={inputs.lastName} onChange={handleChange}></Input>
-                    {errors['lastName'] ? <p className="errors_reg_log">{errors['lastName'].message}</p>: ''}
-                </FormGroup>
+
                 <FormGroup>
                     <Label>Email:</Label>
-                    <Input name='email' value={inputs.email} onChange={handleChange}></Input>
+                    <Input name='email'  onChange={handleChange}></Input>
                     {errors['email'] ? <p className="errors_reg_log">{errors['email'].message}</p>: ''}
                 </FormGroup>
                 <FormGroup>
                     <Label>Birthdate:</Label>
-                    <Input  type="date" name='birthdate' value={inputs.birthdate} onChange={handleChange}></Input>
+                    <Input  type="date" name='birthdate' onChange={handleChange}></Input>
                 </FormGroup>
                 <Button type="submit" onClick={enviarRegistro}>Register</Button>
             </Form>

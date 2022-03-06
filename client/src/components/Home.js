e3cb5d1b4de9bc44d3acf169c0faed7ca7c0ec3e
Niag2021 <arancibia.guerrero.nicolas@gmail.com>
2022-03-05 22:39:13 -0300

    se agrego Dashboard en Home y su css en app.csss

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,81 +1,37 @@
-import { Container, Button, Col, Form, FormGroup, Input, Label, Row } from "reactstrap";
+import { Container } from "reactstrap";
 
 
 const Home = (props) => {
     return (
         //<></>
         <Container>
-            <div id="redesSociales">
-                <button id="redesSociales2">F</button>
-                <button id="redesSociales2">Y</button>
-                <button id="redesSociales2">T</button>
-                <button id="redesSociales2">I</button>
-                <button id="redesSociales2">Tw</button>
-
-                <button id="login">Login</button>
+            <div id="header" align="center">
+                <p id="items">Home</p>
+                <p id="items">New</p>
+                <p id="items">Search</p>
+                <p id="items">Account</p>
             </div>
 
-            <div id="parteIzquierda">
-                <div>
+            <div id="centro">
+                <h1 id="titulo">Welcome Minh!</h1>
+                <p id="tituloDiaActual">Listado Actual</p>
 
-                </div>
-            </div>
-            
-            <div id="parteDerecha">
-                <div>
-                    <Form id="formulario">
-                        <tr id="campos">
-                            <td id="texto"> Label 1</td>
-                            <td id="valor">Valor 1</td>
-                            <td id="acciones">Acciones</td>
-                        </tr>
+                <table id="tabla">
+                    <tr>
+                        <td>Campo 1</td>
+                        <td>Campo 2</td>
+                        <td>Campo 3</td>
+                    </tr>
 
-                        <tr id="campos">
-                            <td id="texto"> Label 2</td>
-                            <td id="valor">Valor 2</td>
-                            <td id="acciones">Acciones</td>
-                        </tr>
-
-                        <tr id="campos">
-                            <td id="texto"> Label 4</td>
-                            <td id="valor">Valor 4</td>
-                            <td id="acciones">Acciones</td>
-                        </tr>
-
-                        <tr id="campos">
-                            <td id="texto"> Label 5</td>
-                            <td id="valor">Valor 5</td>
-                            <td id="acciones">Acciones</td>
-                        </tr>
-
-                        <tr id="campos">
-                            <td id="texto"> Label 6</td>
-                            <td id="valor">Valor 6</td>
-                            <td id="acciones">Acciones</td>
-                        </tr>
-
-                        <tr id="campos">
-                            <td id="texto"> Label 7</td>
-                            <td id="valor">Valor 7</td>
-                            <td id="acciones">Acciones</td>
-                        </tr>
-
-                        <tr id="campos">
-                            <td id="texto"> Label 8</td>
-                            <td id="valor">Valor 8</td>
-                            <td id="acciones">Acciones</td>
-                        </tr>
-
-                        <tr id="campos">
-                            <td id="texto"> Label 9</td>
-                            <td id="valor">Valor 9</td>
-                            <td id="acciones">Acciones</td>
-                        </tr>
-                    </Form>
-                </div>
-            </div>
+                    <tr>
+                        <td>Celda 4</td>
+                        <td>Celda 5</td>
+                        <td>Celda 6</td>
+                    </tr>                    
+                </table>
+            </div>            
         </Container>       
-    )
+    );
 }
 
 export default Home;

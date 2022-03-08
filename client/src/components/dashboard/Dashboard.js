330ffdefcd71676bf15959f81d489f3bc7c3a50f
Niag2021 <arancibia.guerrero.nicolas@gmail.com>
2022-03-08 13:38:59 -0300

    centrar titulo en dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,13 +1,14 @@
+import "../../App.css";
 import React from "react";
 import TableToday from "./TableToday";
 
 const Dashboard = () => {
   return (
     <div className="dashboard">
-      <h3>Hoy es Domingo 6 de Mazo y tienes los siguientes eventos:</h3>
+      <h3 id="tituloDashboard">Hoy es Domingo 6 de Mazo y tienes los siguientes eventos:</h3>
       <TableToday />
       <br></br>
-      <h3>Estos son tus futuros eventos:</h3>
+      <h3 id="tituloDashboard">Estos son tus futuros eventos:</h3>
       <TableToday />
     </div>
   );

011ad93e3ae2460f4e53f1aa56c2f29a1bde8b79
diego <entroox@gmail.com>
2022-03-16 01:37:47 -0300

    +key

--- client/src/components/events/DataEventUser.js
+++ client/src/components/events/DataEventUser.js
@@ -4,7 +4,7 @@ const DataEventUser = ({data}) => {
     return (
         <div className='event-user'>
             <h3>Usuarios</h3>
-            {data?.eventMembers.map(t => <p>{t.username} - {t.email} <span className="btn-available">Confirmed</span></p>)}
+            {data?.eventMembers.map(t => <p key={t.username}>{t.username} - {t.email} <span className="btn-available">Confirmed</span></p>)}
         </div>
     );
 };
